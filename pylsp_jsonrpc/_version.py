46f3864703e6f2a9e457b89b9313c7982a36282a
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2021-04-14 16:20:37 -0500

    Back to work

--- pylsp_jsonrpc/_version.py
+++ pylsp_jsonrpc/_version.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2020 Palantir Technologies, Inc.
 # Copyright 2021- Python Language Server Contributors.
 
-VERSION_INFO = (1, 0, 0)
+VERSION_INFO = (1, 1, 0, 'dev0')
 __version__ = '.'.join(map(str, VERSION_INFO))
