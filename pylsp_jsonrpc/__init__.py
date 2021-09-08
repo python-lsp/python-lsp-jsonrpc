ae2ac4489158a91f24083056230286bf81ec41d6
KOLANICH <kolan_n@mail.ru>
2021-09-08 12:28:41 +0300

    Moved the metadata into `setup.cfg` Added `pyproject.toml` Replaced fetching version from a file with fetching a version from version control system tags. Also fixed a bug with incorrect content of `__all__` in `__main__.py`

--- pylsp_jsonrpc/__init__.py
+++ pylsp_jsonrpc/__init__.py
@@ -1,6 +1,23 @@
 # Copyright 2017-2020 Palantir Technologies, Inc.
 # Copyright 2021- Python Language Server Contributors.
 
+from . import _version
 from ._version import __version__
 
-__all__ = [__version__]
+
+def convert_version_info(version: str) -> (int, ..., str):
+    version_info = version.split(".")
+    for i in range(len(version_info)):  # pylint:disable=consider-using-enumerate
+        try:
+            version_info[i] = int(version_info[i])
+        except ValueError:
+            version_info[i] = version_info[i].split("+")[0]
+            version_info = version_info[: i + 1]
+            break
+
+    return tuple(version_info)
+
+
+_version.VERSION_INFO = convert_version_info(__version__)
+
+__all__ = ("__version__",)
